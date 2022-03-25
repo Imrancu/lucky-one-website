73ade0036ccfecde2a69d7201b0f67e8d6435069
imrancu <mdimran3772@gmail.com>
2022-03-26 02:23:48 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Animals World</h1>
+      <p>An animal's eyes have the power to speak a great language!</p>
+    </div>
+  );
+};
+
+export default Header;
